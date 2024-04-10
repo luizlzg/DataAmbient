1ec1154645417f03b8c37b4a0f284c8eafc35416
Luiz Guilherme Corrêa <63373640+luizlzg@users.noreply.github.com>
2024-04-10 14:47:34 -0300

    Update image_process.py

--- processing/image_process.py
+++ processing/image_process.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
 
+# Função que calcula a distância cosseno. Ela é utilizada para a calcular a distância entre 2 faces, de modo que quanto mais próximo de 0, mais as faces se parecem.
 def cosine_similarity(source_representation, test_representation):
     a = np.matmul(np.transpose(source_representation), test_representation)
     b = np.sum(np.multiply(source_representation, source_representation))
     c = np.sum(np.multiply(test_representation, test_representation))
     return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
 
-def get_image_from_camera():
-    pass
-
-
+# Função responsável por processar a imagem proveniente da câmera, detectando e retornando as faces presentes. Caso não haja nenhuma face, é retornado False.
 def process_image(img, mtcnn, resnet):
-    faces, probs = mtcnn(img, return_prob=True)
+    faces, probs = mtcnn(img, return_prob=True) # detectando faces na imagem/frame
     if probs[0] is None:
         return False, None
     indices = []
@@ -25,7 +23,7 @@ def process_image(img, mtcnn, resnet):
         return True, faces_returned
 
 
-# Função para converter a coluna 'embedding' em um array NumPy
+# Função para converter a coluna 'embedding' (armazena o embedding no banco de dados) em um array NumPy
 def convert_embedding(embedding):
     """
     Essa função converte o embedding, que está no formato de bytes, para um array NumPy.
@@ -35,6 +33,7 @@ def convert_embedding(embedding):
     return np.frombuffer(embedding, dtype=np.float32)
 
 
+# Função que obtém o embedding da face.
 def get_face_embedding_rt(img_cropped, resnet, device):
     img_embedding = resnet(img_cropped.unsqueeze(0).to(device))
 
